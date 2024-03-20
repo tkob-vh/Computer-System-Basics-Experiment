8e17b4cfda5fc89df500e6118fea9746fdbbc83c
yyx <yuxin.yan.cs@gmail.com>
2024-03-20 14:58:40 +0000

    feat: finish bitMask and byteSwap

---
+++ lab1/task2/bitMask.c
@@ -0,0 +1,16 @@
+int bitMask(int lowbit, int highbit);
+int bitMask_standard(int lowbit, int highbit);
+
+int bitMask(int lowbit, int highbit){
+    int all1 = ~0;
+    int mask = all1 + (2 << highbit);
+    return (mask >> lowbit) << lowbit;
+}
+
+int bitMask_standard(int lowbit, int highbit){
+    int mask = 0;
+    for(int i = lowbit; i <= highbit; i++){
+        mask |= 1 << i;
+    }
+    return mask;
+}
